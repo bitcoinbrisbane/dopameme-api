651ddf7c15579b249eaee16066722c3a1da13583
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-05-16 14:31:17 +1000

    feat: add packages, scripts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ const Chats = require("./models/chat");
 const Coins = require("./models/coin");
 const Profile = require("./models/profile");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const connectDB = require("./config/dbConfig");
+connectDB();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const check_signature = process.env.CHECK_SIGNATURE || false;
 
 app.get("/", (req, res) => {
   res.send("dopeameme!");
@@ -44,6 +57,11 @@ app.get("/profile/:address", async (req, res) => {
   res.json(profile);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+mongoose.connection.once("open", () => {
+  app.listen(PORT, async () => {
+    // const client = new MongoClient(uri);
+    // await client.connect();
+    console.log(`PVM server running on port ${PORT}`);
+  });
 });
