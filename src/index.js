a13ed595929e826894a4bbe7ceafd98ee1e580da
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-05-29 11:30:08 +1000

    chore: Add PORT variable to .env.example and update routes for coins

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const swagger = require("./swagger");
-const Chat = require("./models/chat");
-const Coins = require("./models/coin");
+
 const Profile = require("./models/profile");
 
 const dotenv = require("dotenv");
@@ -24,9 +23,6 @@ swagger(app);
 
 const check_signature = process.env.CHECK_SIGNATURE || false;
 
-// Routes
-app.use("/chat", chat);
-
 /**
  * @swagger
  * /:
@@ -40,32 +36,10 @@ app.get("/", (req, res) => {
   res.send("dopeameme!");
 });
 
-app.get("/coins/", async (req, res) => {
-  const coins = await Coins.find();
-  res.json(coins);
-});
 
-app.get("/coins/:address", async (req, res) => {
-  const address = req.params.address;
-  const coin = await Coins.findOne({ address });
-
-  res.json(coin);
-});
-
-app.get("/coins/hodlers/:address", async (req, res) => {
-  const wallets = [
-    {
-      address: "0x1234",
-      amount: 100,
-    },
-    {
-      address: "0x5678",
-      amount: 200,
-    },
-  ];
-
-  return res.json(wallets);
-});
+// Routes
+app.use("/chat", chat);
+app.use("/coins", require("./routes/coins"));
 
 app.get("/profile/:address", async (req, res) => {
   const address = req.params.address;
--- src/routes/coins.js
+++ src/routes/coins.js
@@ -1,2 +1,59 @@
 // add router for coins
+const express = require("express");
+const router = express.Router();
 
+const Coins = require("./models/coin");
+
+router.get("/coins/", async (req, res) => {
+  const coins = await Coins.find();
+  res.json(coins);
+});
+
+router.get("/coins/next/", async (req, res) => {
+    const coins = await Coins.find();
+
+    // get a random coin
+    const randomIndex = Math.floor(Math.random() * coins.length);
+    const randomCoin = coins[randomIndex];
+
+    res.json(randomCoin);
+  });
+
+router.get("/coin/:address", async (req, res) => {
+  const address = req.params.address;
+  const coin = await Coins.findOne({ address });
+
+  res.json(coin);
+});
+
+router.get("/coin/hodlers/:address", async (req, res) => {
+  const wallets = [
+    {
+      address: "0x1234",
+      amount: 100,
+    },
+    {
+      address: "0x5678",
+      amount: 200,
+    },
+  ];
+
+  return res.json(wallets);
+});
+
+router.get("/coin/updates/:address", async (req, res) => {
+  const updates = [
+    {
+      update: "Lipsum dolor sit amet",
+      date: date.now(),
+    },
+    {
+      update: "Lipsum dolor sit amet",
+      date: date.now(),
+    },
+  ];
+
+  return res.json(updates);
+});
+
+module.exports = router;
